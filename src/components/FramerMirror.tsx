ed14615d0f95893738be3b98948dfa06f2fcc938
Uday Surya P <udaysuryap.workspace@gmail.com>
2026-06-04 00:55:53 +0530

    Revert "Preserve responsive iframe width"
    This reverts commit 13a9b9f62e82dbdec4ad061dbd18e327455b0afc.

--- src/components/FramerMirror.tsx
+++ src/components/FramerMirror.tsx
@@ -9,11 +9,10 @@ export default function FramerMirror({ route }: { route: string }) {
       <iframe
         title="Humaneframes"
         src={src}
-        className="h-dvh w-screen border-0 bg-[#f3f3f1]"
+        className="h-dvh w-[calc(100vw+18px)] border-0 bg-[#f3f3f1]"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         referrerPolicy="strict-origin-when-cross-origin"
       />
-      <div aria-hidden="true" className="pointer-events-none absolute inset-y-0 right-0 w-[18px] bg-[#f3f3f1]" />
     </div>
   );
 }
